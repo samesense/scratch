809a598b2adbf28f0b52142ad0f40f53f7c5b02d
Perry <samesense@gmail.com>
2010-06-16 22:29:26 -0400

    flu rnai test

---
+++ flu_rnai_hubs.py
@@ -0,0 +1,48 @@
+"""At this point, I am questioning the
+   conclusion that RNAi data is enriched
+   in hubs. I have shown that HCV RNAi
+   hits that are genes that are bad
+   for the virus (probably immune genes)
+   are not enriched in hubs.
+
+   I want to test the same thing for flu.
+   This is complicated by the Z-scores
+   that are provided in the Cell article
+   w/ Aviv Regev as co author. I'll have
+   to pick an arbitrary cutoff, and look
+   at hub enrichment compared to all hits
+   in the file.  All hits is not the best
+   way to go; really I need to ~1700 genes
+   that were tested & could be knocked
+   down w/o killing the cell, but these
+   are not available.
+"""
+import utils_stats, utils_graph
+
+flu_rnai_file = '../Thesis/Data/Network/Flu/cell09/all_rnai'
+hubs_file = '../Thesis/Data/Hubs2/HPRD.entrez.expand.hubs20'
+net_file = '../Thesis/Data/Network/Human/HPRD/hprd_new.intr.ls.entrez'
+
+network_genes = set(utils_graph.getNodes(net_file))
+hubs = set(utils_graph.getNodes(hubs_file))
+
+all_rnai = {}
+replication_rnai = {}
+with open(flu_rnai_file) as f:
+    for line in f:
+        [entrez, delNS1, 
+         vRNA, replication] = [float(x) for x in line.strip().split('\t')]
+        ID = str(int(entrez))
+        if replication < float(0):
+            replication_rnai[ID] = True
+        all_rnai[ID] = True
+
+bg = set(network_genes & set(all_rnai.keys()))
+print 'background', len(bg)
+print 'hubs in background', len(bg & hubs)
+print 'replication bg', len(set(replication_rnai.keys()) & bg)
+print 'replication hubs', len(hubs & set(replication_rnai.keys()))
+
+print utils_stats.fisher_positive_pval([len(bg & hubs), len(bg) - len(bg & hubs)],
+                                       [len(hubs & set(replication_rnai.keys())),
+                                        len(set(replication_rnai.keys()) & bg) - len(hubs & set(replication_rnai.keys()))])
